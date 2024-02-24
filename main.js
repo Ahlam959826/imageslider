96d2e2d7c73b866aeae632cf65bd1be0d5548284
Ahlam959826- <ruusahmed296@gmail>
2024-02-24 02:20:17 -0800

    tijaabao

---
+++ main.js
@@ -0,0 +1,100 @@
+
+/*
+let box = document.querySelector("#box")
+let onbtn = document.querySelector("#onbtn")
+let offbtn = document.querySelector("#offbtn")
+
+onbtn.addEventListener("click",function(){
+    box.style.backgroundColor ="yellow"
+})
+
+
+offbtn.addEventListener("click",function(){
+    box.style.backgroundColor ="white"
+})
+
+*/
+
+/*
+let counter = document.querySelector("#counter")
+let incrementbtn = document.querySelector("#incrementbtn")
+let decrementbtn = document.querySelector("#decrementbtn")
+let resetbtn = document.querySelector("#resetbtn")
+let body = document.querySelector("#body")
+
+let initialValue = 0
+incrementbtn.addEventListener("click",function(){
+    initialValue++
+    if (initialValue==3) {
+        body.style.backgroundColor="blue"
+    }
+    else if(initialValue==4){
+        body.style.backgroundColor="red"
+    }
+    else{
+        body.style.backgroundColor="white"
+    }
+    counter.innerHTML = initialValue
+})
+
+// let initialValue2 = -0
+//  decrementbtn.addEventListener("click",function(){
+//     initialValue--
+//     counter.innerHTML = initialValue
+// })
+
+
+
+// qimaaha laa badinaayo
+// let initialValue3 = 0
+// let blue = 7
+// let red = 1
+// let blue2 = 2
+// let green = 3
+// let black = 4
+// let orange = 5
+
+
+// incrementbtn.addEventListener("click",function(){
+//     initialValue++
+//     counter.innerHTML = initialValue
+//     if(initialValue==7){
+//         body.style.backgroundColor = "blue"
+//     }
+
+//     else if(red == initialValue){
+//         body.style.backgroundColor = "red"
+//     }
+
+//     else if(blue2 == initialValue){
+//         body.style.backgroundColor = "blue2"
+//     }
+
+//     else if(green == initialValue){
+//         body.style.backgroundColor = "green"
+//     }
+
+//     else if(black == initialValue){
+//         body.style.backgroundColor = "black"
+//     }
+
+//     else if(orange == initialValue){
+//         body.style.backgroundColor = "orange"
+//     }
+
+//     else{
+//         body.style.backgroundColor = "white"
+//     }
+// })
+
+*/
+
+/*
+
+resetbtn.addEventListener("click",function(){
+    initialValue = 0
+    counter.innerHTML= initialValue
+
+})
+*/
+
